98ae53b8e2f33482383190437c19fc80974c00c0
Gyanu <gyanusherchan@outlook.com>
2023-06-10 23:53:53 +1000

    authmiddleware for protected route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,9 @@ import express from "express";
 import {
   registerController,
   loginController,
+  testController,
 } from "../controllers/authController.js";
+import { requireSignIn } from "../middlewares/authMiddleware.js";
 
 //router
 
@@ -14,5 +16,8 @@ router.post("/register", registerController);
 
 //LOGIN || POST
 router.post("/login", loginController);
+
+//test routes
+router.get("/test", requireSignIn, testController);
 
 export default router;
